cff4c3e21c3aa9e6c244d241549db96db13ecece
Christoph Franke <krito@krito.de>
2019-07-25 15:56:22 +0200

    connected to prismic repository

---
+++ fetch.js
@@ -0,0 +1,18 @@
+const Prismic = require('prismic-javascript')
+const fs = require('fs')
+
+const apiEndpoint = 'https://nuaflow-de.cdn.prismic.io/api/v2'
+const targetFile = `${__dirname}/cms-data.json`
+ 
+Prismic.getApi(apiEndpoint)
+  .then(api => api.query('', { pageSize : 100 }))
+  .then(response => {
+    if (response.total_pages > 1) {
+      console.error('There are more results than fit into first response page. This will lead to missing cms data. Do another api request for the next page to fix this.')
+    }
+    return new Promise(resolve => {
+      fs.writeFile(targetFile, JSON.stringify(response.results), () => resolve())
+    })
+  }).then(() => {
+    console.log(`successfully written cms data to ${targetFile}`)
+  })

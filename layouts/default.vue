937f449f1952d7e72fc3645d60f8700789f9bacd
Christoph Franke <krito@krito.de>
2019-11-13 12:19:46 +0100

    added basic donation modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,21 +16,39 @@
           <iframe type="text/html" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://lesen.amazon.de/kp/card?asin=B07VH5Q2FG&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_4ygtDbNS7NE1X" ></iframe>
           <img src="/amazon-logo.png" class="amazon-logo">
           <br><a :href="pdf.url" download target="_blank" v-if="pdf.url && pdf.label">{{ pdf.label }}</a>
+          <br><span class="donation">Ich freue mich über eine <a href="#" @click.prevent="openModal">Spende</a></span>
         </div>
         <div class="shop">
           <div id="my-store-17735022"></div>
         </div>
       </div>
     </transition>
+
+    <div class="modal" v-if="isModalOpen">
+      <div class="modal__backdrop" @click="closeModal">
+        <div class="modal__window">
+          <RichText :content="$store.getters.shop.donation_modal" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import components from '~/components'
+import PrismicDOM from 'prismic-dom'
+import linkResolver from '~/util/linkResolver'
 
 
 export default {
   components,
+
+  data() {
+    return {
+      isModalOpen: false
+    }
+  },
+
   computed: {
     shopVisible() {
       return this.$route.name === 'meister-eckart-shop'
@@ -44,6 +62,17 @@ export default {
     }
   },
 
+  methods: {
+    openModal() {
+      this.isModalOpen = true
+      document.body.classList.add('modal-open')
+    },
+    closeModal() {
+      this.isModalOpen = false
+      document.body.classList.remove('modal-open')
+    }    
+  },
+
   mounted() {
     if (process.browser) {
       try {
@@ -54,6 +83,8 @@ export default {
       } catch(err) {
         this.$sentry.captureException(err);
       }
+
+      console.log(this.$store.getters.shop)
     }
   }
 }
@@ -62,6 +93,30 @@ export default {
 
 <style lang="scss">
 @import '../style/global.scss';
+
+.modal {
+  &__backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__window {
+    max-height: 80vh;
+    min-height: 250px;
+    width: 60vw;
+    overflow-y: auto;
+    background: rgb(255, 255, 255);
+    padding: 20px;
+    color: black;
+  }
+}
 
 .shop-and-book {
   display: flex;
@@ -110,6 +165,15 @@ export default {
   margin-top: 20px;
   clear: both;
   width: 100%;
+}
+
+.donation a {
+  font-size: 20px;
+}
+
+body.modal-open {
+  overflow:hidden;
+  max-height: 100%;
 }
 
 

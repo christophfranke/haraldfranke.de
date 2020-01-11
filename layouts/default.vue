1c84a9701e643cded647debb3f35cc31ece0252b
Christoph Franke <krito@krito.de>
2020-01-11 17:34:42 +0100

    removed ecwid shop

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,7 @@
 <template>
   <div>
-    <transition name="page">
-      <div class="toolbar" v-show="shopVisible">
-        <div class="ec-cart-widget"></div>
-        <div id="my-search-17735022"></div>
-      </div>
-    </transition>
-
     <Navigation />
     <nuxt/>
-
-    <transition name="page">
-      <div class="shop-and-book" v-show="shopVisible">
-        <div class="book">
-          <iframe type="text/html" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://lesen.amazon.de/kp/card?asin=B07VH5Q2FG&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_4ygtDbNS7NE1X" ></iframe>
-          <img src="/amazon-logo.png" class="amazon-logo">
-          <br><a :href="pdf.url" download target="_blank" v-if="pdf.url && pdf.label">{{ pdf.label }}</a>
-          <br><span class="donation">Ich freue mich über eine <a href="#" @click.prevent="openModal">Spende</a></span>
-        </div>
-        <div class="shop">
-          <div id="my-store-17735022"></div>
-        </div>
-      </div>
-    </transition>
-
-    <div class="modal" v-if="isModalOpen">
-      <div class="modal__backdrop" @click="closeModal">
-        <div class="modal__window" @click.stop>
-          <RichText :content="$store.getters.shop.donation_modal" />
-          <span class="modal__x" @click="closeModal">x</span>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -42,107 +12,13 @@ import linkResolver from '~/util/linkResolver'
 
 
 export default {
-  components,
-
-  data() {
-    return {
-      isModalOpen: false
-    }
-  },
-
-  computed: {
-    shopVisible() {
-      return this.$route.name === 'meister-eckart-shop'
-    },
-
-    pdf() {
-      return {
-        url: this.$store.getters.shop.die_lehren_meister_eckarts_pdf && this.$store.getters.shop.die_lehren_meister_eckarts_pdf.url,
-        label: this.$store.getters.shop.pdf_download_label && this.$store.getters.shop.pdf_download_label[0].text
-      }
-    }
-  },
-
-  methods: {
-    openModal() {
-      this.isModalOpen = true
-      document.body.classList.add('modal-open')
-    },
-    closeModal() {
-      this.isModalOpen = false
-      document.body.classList.remove('modal-open')
-    }    
-  },
-
-  mounted() {
-    if (process.browser) {
-      try {
-        Ecwid.init()
-        xSearch("id=my-search-17735022")
-        xCategoriesV2("id=my-categories-17735022")
-        xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-17735022")
-      } catch(err) {
-        this.$sentry.captureException(err);
-      }
-
-      console.log(this.$store.getters.shop)
-    }
-  }
+  components
 }
 </script>
 
 
 <style lang="scss">
 @import '../style/global.scss';
-
-.modal {
-  &__backdrop {
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 2;
-  }
-
-  &__window {
-    max-height: 80vh;
-    min-height: 250px;
-    width: 50vw;
-    overflow-y: auto;
-    padding: 10px 30px 30px 30px;
-    background-color: $background-color;
-    border: 6px solid $image-border-color;
-    position: relative;
-    @media (max-width: 650px) {
-      width: 80vw;
-      margin: 0 10px;
-    }
-    @media (max-width: 400px) {
-      width: 100vw;
-      padding: 10px 10px 10px 10px;
-      font-size: 18px;
-    }
-  }
-
-  &__x {
-    position: absolute;
-    top: 0;
-    right: 0;
-    font-size: 30px;
-    display: block;
-    width: 30px;
-    height: 30px;
-    cursor: pointer;
-    &:hover {
-      filter: drop-shadow(0px 3px 2px rgba(0,0,0,0.75));
-    }
-  }
-}
 
 .shop-and-book {
   display: flex;
@@ -170,16 +46,6 @@ export default {
     display: inline-block;
     margin-top: 10px;  
   }
-}
-
-.amazon-logo {
-  position: absolute;
-  left: 50%;
-  top: 35%;
-  transform: translateX(-50%);
-  width: 130px;
-  pointer-events: none;
-  border: none;
 }
 
 .toolbar {
--- pages/meister-eckart-shop.vue
+++ pages/meister-eckart-shop.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <RichText :content="page.title" className="full-width" />
+    <div class="book">
+      <iframe type="text/html" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://lesen.amazon.de/kp/card?asin=B07VH5Q2FG&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_4ygtDbNS7NE1X" ></iframe>
+      <img src="/amazon-logo.png" class="amazon-logo">
+      <br><a :href="pdf.url" download target="_blank" v-if="pdf.url && pdf.label">{{ pdf.label }}</a>
+      <br><span class="donation">Ich freue mich über eine <a href="#" @click.prevent="openModal">Spende</a></span>
+    </div>
+
+    <transition name="fade">
+      <div class="modal" v-if="isModalOpen">
+        <div class="modal__backdrop" @click="closeModal">
+          <div class="modal__window" @click.stop>
+            <RichText :content="$store.getters.shop.donation_modal" />
+            <span class="modal__x" @click="closeModal">x</span>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -11,18 +28,104 @@ export default {
   name: 'Shop',
   components,
 
-  computed: {    
-    page() {
-      return this.$store.getters.shop
+  data() {
+    return {
+      isModalOpen: false
     }
   },
 
-  mounted() {
-    if (process.browser) {
-      // this will resize the shop to the needed dimensions when initialized before in hidden mode
-      // otherwise shop layout will look broken
-      window.dispatchEvent(new Event('resize'));
+  methods: {
+    openModal() {
+      this.isModalOpen = true
+      document.body.classList.add('modal-open')
+    },
+    closeModal() {
+      this.isModalOpen = false
+      document.body.classList.remove('modal-open')
+    }    
+  },
+
+  computed: {    
+    page() {
+      return this.$store.getters.shop
+    },
+
+    pdf() {
+      return {
+        url: this.$store.getters.shop.die_lehren_meister_eckarts_pdf && this.$store.getters.shop.die_lehren_meister_eckarts_pdf.url,
+        label: this.$store.getters.shop.pdf_download_label && this.$store.getters.shop.pdf_download_label[0].text
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../style/global.scss';
+.amazon-logo {
+  position: absolute;
+  left: 50%;
+  top: 35%;
+  transform: translateX(-50%);
+  width: 130px;
+  pointer-events: none;
+  border: none;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+
+.modal {
+  &__backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 2;
+  }
+
+  &__window {
+    max-height: 80vh;
+    min-height: 250px;
+    width: 50vw;
+    overflow-y: auto;
+    padding: 10px 30px 30px 30px;
+    background-color: $background-color;
+    border: 6px solid $image-border-color;
+    position: relative;
+    @media (max-width: 650px) {
+      width: 80vw;
+      margin: 0 10px;
+    }
+    @media (max-width: 400px) {
+      width: 100vw;
+      padding: 10px 10px 10px 10px;
+      font-size: 18px;
+    }
+  }
+
+  &__x {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 30px;
+    display: block;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    &:hover {
+      filter: drop-shadow(0px 3px 2px rgba(0,0,0,0.75));
+    }
+  }
+}
+</style>
